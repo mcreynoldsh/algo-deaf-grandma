2570a62b0bb94bd6c96447a384757754787c1f0b
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-24 13:20:45 -0500

    initial

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,4 +1,28 @@
 function deafGrandma() {
+    let goodbyes = 0;
+    let lowerReg= /[a-z]/
+    let response = window.prompt("HEY KID!");
+    
+    while(goodbyes!=2){
+    
+        if(response == "GOODBYE!"){
+            goodbyes++;
+            if(goodbyes===1){
+                response=window.prompt("LEAVING SO SOON?");
+            }    
+        }
+        else if(response===""){
+            response=window.prompt("WHAT?!")
+        }
+        else if(lowerReg.test(response)){
+            response=window.prompt("SPEAK UP, KID!");
+        }
+        else{
+            response=window.prompt("NO, NOT SINCE 1946!");
+        }
+    }
+    window.alert("LATER SKATER!")
+    
 
 }
 
