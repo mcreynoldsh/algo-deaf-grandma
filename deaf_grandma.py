ffe4e7e9c0648133a63964c7a4a1254256111ad4
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-25 10:58:13 -0500

    initial py solution

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,24 @@
+from urllib import response
+import re
+
 def deaf_grandma():
 
+    goodbyes = 0
+    response = input("HEY KID!\n")
 
+    while goodbyes != 2:
+
+        if response == "GOODBYE!":
+            goodbyes+=1
+            if goodbyes == 1:
+                response = input("LEAVING SO SOON?\n")
+        elif response == "":
+            response= input("WHAT?!\n")
+        elif(re.search("[a-z]",response)!=None):
+            response= input("SPEAK UP, KID!\n")
+        else:
+            response = input("NO, NOT SINCE 1946!\n")    
+    
+    print("LATER SKATER!")
 
 deaf_grandma()
